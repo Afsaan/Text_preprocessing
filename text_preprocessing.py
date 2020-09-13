2601bdd754ce0720298eaceeec3efcb3c55d6f64
afsan <khanafsaan11@gmail.com>
2020-09-14 02:14:10 +0530

    basic preprocessing

---
+++ text_preprocessing.py
@@ -0,0 +1,33 @@
+
+class text_preprocess():
+    def __init__(self):
+        pass
+
+    def lower_case(self):
+        pass
+
+    def remove_numbers_from_txt(self):
+        pass
+
+    def remove_punctuation(self):
+        pass
+
+    def remove_stopwords(self):
+        pass
+
+    def stemming(self):
+        pass
+
+    def lemmatization(self):
+        pass
+
+    def pos_tagging(self):
+        pass
+
+    def chunking(self):
+        pass
+
+    def name_entity_recognition(self):
+        pass
+
+    
