f3f809cbd98d94dfd9c60489a7b77b30d6320768
afsan <khanafsaan11@gmail.com>
2020-09-14 02:26:27 +0530

    added re and lower case of string

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -1,12 +1,22 @@
+#importing important libraries
+import nltk
+import string
+import re
+
 
 class text_preprocess():
     def __init__(self):
         pass
 
-    def lower_case(self):
-        pass
+    def lower_case(self , text):
+        print(text.lower())
 
-    def remove_numbers_from_txt(self):
+    def remove_numbers_from_txt(self , text):
+        result = re.sub(r'\d+' , '', text)
+        print(result)
+        
+
+    def convert_num_to_text(self):
         pass
 
     def remove_punctuation(self):
@@ -30,4 +40,8 @@ class text_preprocess():
     def name_entity_recognition(self):
         pass
 
-    
+if __name__ == "__main__":
+    nltk = text_preprocess()
+    nltk.lower_case('Hello WORLD')
+    nltk.remove_numbers_from_txt('hello 4 world')
+
